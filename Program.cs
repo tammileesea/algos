7c368c9dc771010c9ee55c504a12ac3d05225531
Tammi Lee <tammilee219@gmail.com>
2023-12-04 21:15:41 -0800

    been meaning to do this one for a while

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+var x = new MiscOne();
+var one = new int[]{1, 1, 2, 3}; //0
+var resultOne = x.ArrayPairs(one);
+Console.WriteLine("1: {0}", resultOne);
+
+// var two = new int[]{1, 1, 2, 2, 3}; //1
+// var resultTwo = x.ArrayPairs(two);
+// Console.WriteLine("1: {0}", resultTwo);
+
+// var three = new int[]{8, 1, 2, 3, 4, 5, 8, 6, 7, 8, 8, 8, 8}; //3
+// var resultThree = x.ArrayPairs(three);
+// Console.WriteLine("1: {0}", resultThree);
---
+++ src/Misc/misc_one.cs
@@ -0,0 +1,54 @@
+//if I remember this correctly, each pair consists of different numbers
+//eg [1, 1, 2, 2, 3]
+//1 <= fruits[1] <= 10^9
+//{1, 1, 2, 3}
+//{1, 1, 2, 2, 3}
+//{8, 1, 2, 3, 4, 5, 8, 6, 7, 8, 8, 8, 8}
+
+public class MiscOne
+{
+    public int ArrayPairs(int[] array)
+    {
+        var remaining = array.Length;
+        Console.WriteLine("length: {0}", array.Length);
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine("i: {0}", i);
+            if (array[i] == 0)
+            {
+                continue;
+            }
+
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                Console.WriteLine("j: {0}", j);
+                if (array[j] == 0)
+                {
+                    continue;
+                }
+
+                if (array[i] != array[j])
+                {
+                    //this would be considered a pair, so both values will be set to 0 since all vals in array are greater than 1
+                    array[i] = 0;
+                    array[j] = 0;
+
+                    //since this is a crushable pair, we can remove them from the total count
+                    remaining-=2;
+
+                    //since we found a pair, they can essentially be eliminated so we move i forward to the index after j
+                    //break out of nested loop
+                    Console.WriteLine("break");
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+
+        return remaining;
+    }
+}
